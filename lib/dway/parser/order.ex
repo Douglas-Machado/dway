35b9d267a10a5164fc08908ddcb25a9f63fe8868
Douglas Machado <douglas.machado@deliverycenter.com>
2021-09-28 17:42:42 -0300

    adicionando coord do driver

--- lib/dway/parser/order.ex
+++ lib/dway/parser/order.ex
@@ -2,15 +2,20 @@
 defmodule Dway.Parser.Order do
 
   def get_order(order_params) do
-    pickup_coordinates = order_params["pickup"]["coordinates"]
-    pickup = {pickup_coordinates["lat"], pickup_coordinates["long"]}
-    delivery_coordinates = order_params["delivery"]["coordinates"]
-    delivery = {delivery_coordinates["lat"], delivery_coordinates["long"]}
-
-    Haversine.distance(pickup, delivery)
+    Haversine.distance(get_pickup_coord(order_params), get_delivery_coord(order_params))
     |> IO.inspect(label: "HAVERSINE")
 
 
+  end
+
+  def get_pickup_coord(order_params) do
+    pickup_coordinates = order_params["pickup"]["coordinates"]
+    {pickup_coordinates["lat"], pickup_coordinates["long"]}
+  end
+
+  def get_delivery_coord(order_params) do
+    delivery_coordinates = order_params["delivery"]["coordinates"]
+    {delivery_coordinates["lat"], delivery_coordinates["long"]}
   end
 
 end
