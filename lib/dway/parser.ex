5c3dc7a782305b436ca537d299e1f3a347c45aaa
manaranq <manara.quintino@deliverycenter.com>
2021-09-30 16:16:17 -0300

    escolha driver em caso de empate

--- lib/dway/parser.ex
+++ lib/dway/parser.ex
@@ -3,6 +3,8 @@
 defmodule Dway.Parser do
   alias Dway.Parser.{Driver, Order}
 
+  @max_distance_biker 2000
+
   def get_driver_to_pickup_distance(drivers, order) do
     mapa = Driver.get_driver_coord(drivers)
       |> Enum.map(fn {long, lat, driver_id, modal, index} -> {Haversine.distance({long, lat}, Order.get_pickup_coord(order)), driver_id, modal, index} end)
@@ -10,13 +12,17 @@ defmodule Dway.Parser do
 
     case Enum.filter(mapa, fn {_total_distance, _driver_id, modal, _index} -> modal == "b" end) do
       [] -> Enum.filter(mapa, fn {_total_distance, _driver_id, modal, _index} -> modal == "m" end)
-      |> IO.inspect(label: "moto")
-          |> order_drivers()
-      list -> IO.inspect(list, label: "bike")
-      order_drivers(list)
+        |> order_drivers()
+      list -> case Enum.filter(list, fn {total_distance, _driver_id, _modal, _index} -> total_distance <= @max_distance_biker end) do
+        [] -> Enum.filter(mapa, fn {_total_distance, _driver_id, modal, _index} -> modal == "m" end)
+        |> order_drivers()
+        list -> IO.inspect(list, label: "o que tá indo")
+        order_drivers(list)
+        end
+
 
     end
-      |> IO.inspect(label: "Mapa")
+
     #   Enum.reduce(mapa, {99999999, "ss", "aa"}, fn {distancia, _id, _modal} = driver, {acc_distancia, _, _} = acc -> if distancia < acc_distancia do
     #     driver
     #   else
@@ -28,8 +34,18 @@ defmodule Dway.Parser do
 
   def order_drivers(params) do
     params
-      |> Enum.sort()
-      |> IO.inspect(label: "ordem aqui")
+    |> Enum.reduce({9999999999999999, "ss", "aa", 80000000000000}, fn {distancia, _id, _modal, index} = driver,
+                                           {acc_distancia, _, _, acc_index} = acc ->
+      if distancia < acc_distancia do
+        driver
+      else
+        if distancia == acc_distancia && index < acc_index do
+          driver
+        else
+          acc
+        end
+      end
+    end)
+    |> IO.inspect(label: "ordem aqui")
   end
-
 end
