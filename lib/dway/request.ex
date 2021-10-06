c039e1412c6af92b43b173a37c85656af55e9e81
manaranq <manara.quintino@deliverycenter.com>
2021-10-06 14:17:10 -0300

    validaçao tempo de entrega

--- lib/dway/request.ex
+++ lib/dway/request.ex
@@ -2,8 +2,8 @@ defmodule Dway.Request do
 
   alias Dway.Routing.Route
 
-  @url "http://127.0.0.1:5000/route/v1/driving/"
-  # @u "http://router.project-osrm.org/route/v1/driving/"
+  #@url "http://127.0.0.1:5000/route/v1/driving/"
+   @u "http://router.project-osrm.org/route/v1/driving/"
 
   def get_params(driver, order_params) do
     pickup_long = order_params["pickup"]["coordinates"]["long"]
@@ -11,15 +11,16 @@ defmodule Dway.Request do
     delivery_long = order_params["delivery"]["coordinates"]["long"]
     delivery_lat = order_params["delivery"]["coordinates"]["lat"]
 
-    "#{driver.coordinates.long},#{driver.coordinates.lat};#{pickup_long},#{pickup_lat};#{delivery_long},#{delivery_lat}"
+    params = "#{driver.coordinates.long},#{driver.coordinates.lat};#{pickup_long},#{pickup_lat};#{delivery_long},#{delivery_lat}"
     |> request_osrm()
-    |> Enum.map(fn param -> Route.changeset(%Route{driver: driver.id}, param) |> Route.applied_changeset() end)
+    |> valid_time(order_params)
+    Route.changeset(%Route{driver: driver.id}, params) |> Route.applied_changeset()
   end
 
   def request_osrm(string) do
     HTTPoison.start()
-    %{body: body} = HTTPoison.get!(@url <> string)
-    # %{body: body} = HTTPoison.get! @u <> string <> "?overview=false"
+    #%{body: body} = HTTPoison.get!(@url <> string)
+     %{body: body} = HTTPoison.get! @u <> string <> "?overview=false"
     {:ok, content} = Jason.decode(body, keys: :atoms)
     content.routes
     |> Enum.map(fn el ->
@@ -29,5 +30,15 @@ defmodule Dway.Request do
         "pickup_time" => Enum.at(el[:legs], 0)[:duration],
         "delivery_time" => Enum.at(el[:legs], 1)[:duration]}
     end)
+    |> hd()
   end
+  def valid_time(map, order_params) do
+    case map["total_time"] <= order_params["time_window"] do
+      true -> map
+      _ -> IO.puts("Não é possivel roteirizar")
+
+    end
+
+  end
+
 end
